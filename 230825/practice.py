9dbbff474bd623c3cb78a2ce7677a077979f0286
Kang Dae Su <sore503@naver.com>
2023-08-25 17:44:29 +0900

    20230825

--- 230825/practice.py
+++ 230825/practice.py
@@ -11,7 +11,7 @@ for i in range(N):
             sumV = nums[i]+nums[j]+nums[k]
             if sumV <= M:
                 result = max(result, sumV)
-                break
+
                 
 print(result)
 
