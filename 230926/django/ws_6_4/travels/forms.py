10af366684822e43cbeb48db5ae8e092ecd2ab88
Kang Dae Su <sore503@naver.com>
2023-09-27 13:44:11 +0900

    commit

---
+++ 230926/django/ws_6_4/travels/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Travels
+
+class TravelsForm(forms.ModelForm):
+    location = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'ex) 제주도',
+            }
+        )
+    )
+    start_date = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                'placeholder':'ex) 2022-02-22',
+            }
+        )
+    )
+    end_date = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                'placeholder':'ex) 2022-02-22',
+            }
+        )
+    )
+    class Meta:
+        model = Travels
+        fields = '__all__'
